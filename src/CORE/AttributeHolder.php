9dd71c2acd774a6649c2a760673b8d983f902184
Mihai Balalau <mihaibalalau@gmail.com>
2020-08-23 04:21:52 +0300

    attribute holder fill option

--- src/CORE/AttributeHolder.php
+++ src/CORE/AttributeHolder.php
@@ -6,16 +6,18 @@ abstract class AttributeHolder
 {
     private $attributes = [];
 
-    public function attributes($key = null, $value = null)
+    public function attributes($key = null, $value = null, $fill = false)
     {
         $arg_count = func_num_args();
         switch ($arg_count) {
             case 2:
-                $this->attributes[$key] = $value;
-                break;
+                if ($fill) {
+                    return $this->attributes = $value;
+                } else {
+                    return $this->attributes[$key] = $value;
+                }
             case 1:
                 return isset($this->attributes[$key]) ? $this->attributes[$key] : null;
-                break;
             default:
                 return $this->attributes;
         }
